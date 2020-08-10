d9e5327c53539a6d312e7e62a8b0befa4c69a12f
iamolivermn <mitchelloliver@gmail.com>
2020-08-10 08:00:17 -0700

    Restructure the whole web app and now made the code alot cleaner to read

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-const submitBtnOne = document.querySelector('.submitBtnOne'); // this btn will be linked to your settings submit
-const submitBtnTwo = document.querySelector('.submitBtnTwo'); // this btn will be linked to your settings submit
+// The groups of People
+const people = [];
 
-submitBtnOne.addEventListener('click', createPersonObject);
-// submitBtnTwo.addEventListener('click')
-
-
-let person = {
-    name: '',
-    lowestBill: 0,
-    mediumBill: 0,
-    highestBill: 0,
-    tipOne: 0,
-    tipTwo: 0,
-    tipThree: 0,
-};
-
-
-//functions
-function createPersonObject() {
-    const result = new person();
-    result.name = document.getElementById('name').value;
-    result.lowestBill = document.getElementById('lowBill').value;
-    result.mediumBill = document.getElementById('medBill').value;
-    result.highestBill = document.getElementById('highBill').value;
-    result.tipOne = document.getElementById('tip1').value;
-    result.tipTwo = document.getElementById('tip2').value;
-    result.tipThree = document.getElementById('tip3').value;
+// Functions
+saveNewPerson = (form) => {
+    event.preventDefault();
+    const name = form.name.value;
+    const lowBill = form.lowBill.value;
+    people.push({
+        'name': name,
+        'lowestbill': lowBill
+    })
+    form.name.value = " ";
+    form.lowBill.value = " ";
+    console.log(people);
 }
-
-
-const newPerson = document.getElementById('name').value;
