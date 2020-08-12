9b0465d24b4328d41d0ffcae6396d769806233ce
iamolivermn <mitchelloliver@gmail.com>
2020-08-12 08:12:50 -0700

    Major updates to functions to run, collecting bills and colecting tips

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-// The groups of People
+// Arrays 
 const people = [];
+const bills = [];
+const tips = [];
 
 // Functions
 
-// function to create new person with all there settings in place ;
+// function to create new person object and push in to the people array 
 saveNewPerson = (form) => {
     event.preventDefault();
-    // values from the forms
-    const name = form.name.value;
-    const lowBill = form.lowBill.value;
-    const medBill = form.medBill.value;
-    const highBill = form.highBill.value;
-    const tip1 = form.tip1.value / 100;
-    const tip2 = form.tip2.value / 100;
-    const tip3 = form.tip3.value / 100;
+    // values from the forms that are assign to the object
+    let name = form.name.value;
+    let lowBill = form.lowBill.value;
+    let medBill = form.medBill.value;
+    let highBill = form.highBill.value;
+    let tip1 = form.tip1.value / 100;
+    let tip2 = form.tip2.value / 100;
+    let tip3 = form.tip3.value / 100;
 
+
+    // this push the properties to the PEOPLE array
     people.push({
         'name': name,
         'lowest': lowBill,
@@ -24,9 +28,10 @@ saveNewPerson = (form) => {
         'tip2': tip2,
         'tip3': tip3
     })
-
     console.log(people);
 
+
+    // this reset the forms values 
     form.name.value = "  ";
     form.lowBill.value = " ";
     form.medBill.value = " ";
@@ -35,21 +40,33 @@ saveNewPerson = (form) => {
     form.tip2.value = " ";
     form.tip3.value = " ";
 
-    // tips calculations functions 
-    const tips = [];
+    // The parameters should be the information from the bills array 
+    tipCalculation();
+}
 
-    tipCalculation(45);
 
-    function tipCalculation(bill) {
+// function to collect restaurant bills from the user and push it to the array
+function collectBills(form) {
+    bills.push(form.billAmount.value);
+    form.billAmount.value = " ";
+    form.restaurants.value = " ";
+    console.log(' these are the collect bills' + bills);
+}
 
-        if (bill <= lowBill) {
-            let tip = tip1 * bill;
-        } else if (bill > lowBill && bill < highBill) {
-            let tip = tip2 * bill;
+function tipCalculation(bill) {
+    for (let i = 0; i < people.length; i++) {
+
+        if (bill <= people[i].lowest) {
+            let tip = people[i].tip1 * bill;
+            tips.push(tip);
+        } else if (bill > people[i].lowest && bill < people[i].high) {
+            let tip = people[i].tip2 * bill;
+            tips.push(tip);
         } else {
-            let tip = tip3 * bill;
+            let tip = people[i].tip3 * bill;
+            tips.push(tip);
         }
-        tips.push(tip);
+
     }
     console.log(tips);
 }
