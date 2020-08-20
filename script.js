7b38cc7f6ace8e2814d8f152cf6e2b654dfcab7e
Mitch4sho <mitchelloliver1991@gmail.com>
2020-08-20 16:05:21 -0700

    added functions to display elements in output section

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const billsAndTips = [];
 
 // function to create new person object and push in to the people array 
 saveNewPerson = (form) => {
+
     event.preventDefault();
     // values from the forms that are assign to the object
     let name = form.name.value;
@@ -17,7 +18,6 @@ saveNewPerson = (form) => {
     let tip2 = form.tip2.value / 100;
     let tip3 = form.tip3.value / 100;
 
-
     // this push the properties to the PEOPLE array
     people.push({
         'name': name,
@@ -25,10 +25,9 @@ saveNewPerson = (form) => {
         'high': highBill,
         'tip1': tip1,
         'tip2': tip2,
-        'tip3': tip3
+        'tip3': tip3,
     })
     console.log(people);
-
 
     // this reset the forms values 
     form.name.value = "  ";
@@ -37,6 +36,14 @@ saveNewPerson = (form) => {
     form.tip1.value = " ";
     form.tip2.value = " ";
     form.tip3.value = " ";
+
+    // this creates a new element in the output container for the name
+    for (let f = 0; f < people.length; f++) {
+        var para = document.createElement("P");
+        var t = document.createTextNode("Name: " + people[f].name);
+        para.appendChild(t);
+        document.getElementById('output').appendChild(para);
+    }
 }
 
 function tipCalculation(bill) {
@@ -51,9 +58,7 @@ function tipCalculation(bill) {
             let tip = people[i].tip3 * bill;
             tips.push(tip);
         }
-        var person = people[i].name;
     }
-    document.getElementById("name").innerHTML = person;
     console.log('these are the collected tips ' + tips);
 }
 
@@ -72,7 +77,13 @@ function collectBills(form) {
     let totalBill = parseInt(bill) + parseInt(tips);
     billsAndTips.push(totalBill);
     console.log("All your bills " + billsAndTips);
-    document.getElementById("bill").innerHTML = bills;
-    document.getElementById("tip").innerHTML = tips;
-    document.getElementById("billsAndTips").innerHTML = billsAndTips;
+}
+// a function to create a new element in the output container 
+function myFunction() {
+    for (let f = 0; f < people.length; f++) {
+        var li1 = document.createElement('li');
+        var t = document.createTextNode("Your bills: " + bill[f]);
+        para.appendChild(t);
+        document.getElementById('output').appendChild(para);
+    }
 }
